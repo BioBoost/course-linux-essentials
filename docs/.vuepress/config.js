a0a5d5022e112826dd77927ea0ec829b02b607e1
Nico De Witte <nico.dewitte@outlook.com>
2022-09-23 20:44:00 +0200

    Upgrade to Vuepress 2 - only works with current package-lock.json file

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,188 +1,129 @@
+const { containerPlugin } = require('@vuepress/plugin-container')
+const { defaultTheme } = require('@vuepress/theme-default')
+const { registerComponentsPlugin } = require('@vuepress/plugin-register-components')
+const { path } = require('@vuepress/utils')
+
 module.exports = {
+  lang: 'en-US',
   title: 'Linux Essentials',
   description: 'Linux Essentials Course for VIVES University of Applied Sciences (Bachelor Degree)',
-  themeConfig: {
-    nav: [
-      {text: 'Toledo', link: 'https://toledo.kuleuven.be/portal'},
-      {text: 'DevBit Courses', link: 'https://www.devbit.courses/'},
-      {text: 'Report Issue', link: 'https://github.com/BioBoost/course_linux_essentials/issues'},
-      // {text: 'Challenges', link: 'https://github.com/BioBoost/challenges-linux-essential'}
+
+  theme: defaultTheme({
+    logo: '/images/linux.png',
+    navbar: [
+      { text: 'Toledo', link: 'https://toledo.kuleuven.be/portal' },
+      { text: 'Report Issue', link: 'https://github.com/BioBoost/course_linux_essentials/issues' },
+      { text: 'Organization', link: 'https://github.com/organizations/vives-linux-essentials-2022-2023' }
     ],
     sidebar: [
       {
-        title: 'About this Course',   // required
-        path: '/about-this-course/',      // optional, which should be a absolute path.
-        collapsable: false, // optional, defaults to true
-        sidebarDepth: 1,    // optional, defaults to 1
+        text: 'About this Course',
+        link: '/about-this-course/README.md',
       },
       {
-        title: 'Introductory',   // required
-        path: '/a-introductory/',      // optional, which should be a absolute path.
-        collapsable: false, // optional, defaults to true
-        sidebarDepth: 1,    // optional, defaults to 1
+        text: 'Introduction to Programming',
         children: [
-          '/a-introductory/01-introduction-to-linux/',
-          '/a-introductory/02-start-using-linux/',
-          '/a-introductory/03-the-command-line/',
-          '/a-introductory/04-getting-help/',
+          '/a-introductory/01-introduction-to-linux/README.md',
+          '/a-introductory/02-start-using-linux/README.md',
+          '/a-introductory/03-the-command-line/README.md',
+          '/a-introductory/04-getting-help/README.md',
         ]
       },
       {
-        title: 'Managing Linux Systems',   // required
-        path: '/b-managing-linux-systems/',      // optional, which should be a absolute path.
-        collapsable: false, // optional, defaults to true
-        sidebarDepth: 1,    // optional, defaults to 1
+        text: 'Managing Linux Systems',
         children: [
-          '/b-managing-linux-systems/01-package-managers/',
-          '/b-managing-linux-systems/02-users-and-groups/',
+          '/b-managing-linux-systems/01-package-managers/README.md',
+          '/b-managing-linux-systems/02-users-and-groups/README.md',
         ]
       },
       {
-        title: 'The Linux Filesystem',   // required
-        path: '/c-the-filesystem/',      // optional, which should be a absolute path.
-        collapsable: false, // optional, defaults to true
-        sidebarDepth: 1,    // optional, defaults to 1
+        text: 'The Linux Filesystem',
         children: [
-          '/c-the-filesystem/01-the-filesystem/',
-          '/c-the-filesystem/02-ownership-and-permissions/',
-          '/c-the-filesystem/03-locating-things/',
+          '/c-the-filesystem/01-the-filesystem/README.md',
+          '/c-the-filesystem/02-ownership-and-permissions/README.md',
+          '/c-the-filesystem/03-locating-things/README.md',
         ]
       },
       {
-        title: 'Making Linux Your Own',   // required
-        path: '/d-making-linux-your-own/',      // optional, which should be a absolute path.
-        collapsable: false, // optional, defaults to true
-        sidebarDepth: 1,    // optional, defaults to 1
+        text: 'Making Linux Your Own',
         children: [
-          '/d-making-linux-your-own/01-making-linux-your-own/',
+          '/d-making-linux-your-own/01-making-linux-your-own/README.md',
         ]
       },
       {
-        title: 'Command Line Hacking',   // required
-        path: '/e-command-line-hacking/',      // optional, which should be a absolute path.
-        collapsable: false, // optional, defaults to true
-        sidebarDepth: 1,    // optional, defaults to 1
+        text: 'Command Line Hacking',
         children: [
-          '/e-command-line-hacking/01-chaining-and-redirection/',
-          // '/11-text-manipulation/',
+          '/e-command-line-hacking/01-chaining-and-redirection/README.md',
+          ///11-text-manipulation
         ]
       },
       {
-        title: 'Servers and Services',   // required
-        path: '/f-services/',      // optional, which should be a absolute path.
-        collapsable: false, // optional, defaults to true
-        sidebarDepth: 1,    // optional, defaults to 1
+        text: 'Servers and Services',
         children: [
-          '/f-services/01-remote-access/',
-          '/f-services/02-docker-for-linux/',
-          // WebServer, NAS, Samba, ...
+          '/f-services/01-remote-access/README.md',
+          '/f-services/02-docker-for-linux/README.md',
+          //  // WebServer, NAS, Samba, ...
         ]
       },
       {
-        title: 'Scripting',   // required
-        path: '/g-scripting/',      // optional, which should be a absolute path.
-        collapsable: false, // optional, defaults to true
-        sidebarDepth: 1,    // optional, defaults to 1
+        text: 'Scripting',
         children: [
-          '/g-scripting/01-basic-shell-scripting/',
-          '/g-scripting/02-cron-jobs/',
-      //     // TODO: '/g-scripting/03-systemd/',
+          '/g-scripting/01-basic-shell-scripting/README.md',
+          '/g-scripting/02-cron-jobs/README.md',
+          // /g-scripting/03-systemd
+          // backups
         ]
       },
       {
-        title: 'Security and Protection',   // required
-        path: '/h-security-and-protection/',      // optional, which should be a absolute path.
-        collapsable: false, // optional, defaults to true
-        sidebarDepth: 1,    // optional, defaults to 1
+        text: 'Security and Protection',
         children: [
-          '/h-security-and-protection/01-uncomplicated-firewall/',
+          '/h-security-and-protection/01-uncomplicated-firewall/README.md',
           // Firewall
           // Backups (include creating tar archives and such)
           // OpenVPN
           // ...
         ]
       },
-      // Not sure about how to call this chapter. Or maybe should be split up.
       {
-        title: 'Guides',
-        path: '/x-guides/',      // optional, which should be a absolute path.
-        collapsable: false, // optional, defaults to true
-        sidebarDepth: 1,    // optional, defaults to 1
+        text: 'Guides',
         children: [
-          '/x-guides/01-linux-on-the-rpi/',
-          '/x-guides/02-linux-as-a-vm/',
-          '/x-guides/03-wsl/',
+          '/x-guides/01-linux-on-the-rpi/README.md',
+          '/x-guides/02-linux-as-a-vm/README.md',
+          '/x-guides/03-wsl/README.md',
         ]
       },
       {
-        title: 'References',
-        path: '/z-references/',      // optional, which should be a absolute path.
-        collapsable: false, // optional, defaults to true
-        sidebarDepth: 1,    // optional, defaults to 1
+        text: 'References',
         children: [
-          '/z-references/01-crazy-command-list/',
+          '/z-references/01-crazy-command-list/README.md',
         ]
       },
       {
-        title: 'Online Tools',
-        // path: '/z-references/',      // optional, which should be a absolute path.
-        collapsable: false, // optional, defaults to true
-        sidebarDepth: 1,    // optional, defaults to 1
+        text: 'Online Tools',
         children: [
           ['http://bashrcgenerator.com', '.bashrc PS1 generator'],
           ['https://explainshell.com', 'explainshell.com'],
-          ['https://crontab.guru/', 'crontab guru'],
+          ['https://crontab.guru', 'crontab guru'],
         ]
       },
     ],
     sidebarDepth: 1,
-    repo: 'BioBoost/course_linux_essentials.git',
+    repo: 'BioBoost/course_linux_essentials',
     docsDir: 'docs',
-    docsBranch: 'master',
-    lastUpdated: 'Last Updated', // string | boolean
-    smoothScroll: true
-  },
-  markdown: {
-    lineNumbers: true,
-  },
+    docsBranch: 'master'
+  }),
   serviceWorker: true,
   plugins: [
-    ['vuepress-plugin-zooming', {
-      // selector for images that you want to be zoomable
-      selector: 'img',      // default: '.content img'
-      options: {
-        bgColor: 'black',
-        zIndex: 10000,
+    containerPlugin({
+      type: 'codeoutput',
+      locales: {
+        '/': {
+          defaultInfo: 'Output',
+        },
       },
-    }],
-    ['container', {
-      type: 'output',
-      defaultTitle: 'Output',
-    }],
-    ['container', {
-      type: 'definition',
-      defaultTitle: 'Definition',
-    }],
-    ['container', {
-      type: 'kata',
-      defaultTitle: 'Code Kata',
-    }],
-    ['container', {
-      type: 'question',
-      defaultTitle: 'Question',
-    }],
-    ['container', {
-      type: 'insight',
-      defaultTitle: 'Key Insight',
-    }],
-    ['vuepress-plugin-code-copy', {
-      // selector: String,
-      align: 'bottom',
-      // color: '#DC322F',
-      // backgroundTransition: Boolean,
-      // backgroundColor: String,
-      // successText: String,
-      staticIcon: true,
-    }],
+    }),
+    registerComponentsPlugin({
+      componentsDir: path.resolve(__dirname, './components'),
+    }),
   ],
 }
--- docs/index.md
+++ docs/index.md
@@ -1,12 +1,24 @@
 ---
 home: true
-heroImage: https://avatars3.githubusercontent.com/u/47454925?s=200&v=4
+navbar: false
+heroImage: /images/devbit.png
+heroText: Linux Essentials
 tagline: Linux Essentials Course for VIVES University of Applied Sciences (Bachelor Degree)
-actionText: Get Started →
-actionLink: /about-this-course/
+actions:
+  - text: Get Started →
+    link: /about-this-course/
+    type: primary
+  # - text: Introduction
+  #   link: /guide/
+  #   type: secondary
+# features:
+# - title: Basic Programming Concepts
+#   details: Learn the basic programming concepts found in almost any programming language.
+# - title: Object Oriented Programming
+#   details: Build custom classes, create objects and learn how an object oriented application is build.
+# - title: Graphical User Interface
+#   details: .NET allows modern graphical applications to be build using the Windows Presentation Foundation framework.
 footer: Made by the Nico De Witte with ❤️
 ---
-
-## License
 
 <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
